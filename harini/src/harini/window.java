d881db7158f29a728d9a8a8c9f51af12d6f201b7
on linf <on linf@DESKTOP-PTDLDNV>
2024-03-14 14:38:44 +0530

    hfth5e

---
+++ harini/src/harini/window.java
@@ -0,0 +1,118 @@
+package harini;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.event.ActionListener;
+import java.beans.Statement;
+import java.awt.event.ActionEvent;
+
+public class window {
+
+	private JFrame frame;
+	private JTextField t1;
+	private JTextField t2;
+	private JTextField t3;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					window window = new window();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public window() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("id");
+		lblNewLabel.setBounds(26, 31, 46, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("name");
+		lblNewLabel_1.setBounds(26, 72, 46, 14);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("branch");
+		lblNewLabel_2.setBounds(35, 116, 52, 21);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("marks");
+		lblNewLabel_3.setBounds(35, 160, 46, 14);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		t1 = new JTextField();
+		t1.setBounds(121, 28, 86, 20);
+		frame.getContentPane().add(t1);
+		t1.setColumns(10);
+		
+		t2 = new JTextField();
+		t2.setBounds(131, 69, 86, 20);
+		frame.getContentPane().add(t2);
+		t2.setColumns(10);
+		
+		t3 = new JTextField();
+		t3.setBounds(141, 157, 86, 20);
+		frame.getContentPane().add(t3);
+		t3.setColumns(10);
+		
+		JComboBox cb1 = new JComboBox();
+		cb1.setModel(new DefaultComboBoxModel(new String[] {"select", "cse", "ds", "ece", "eee"}));
+		cb1.setBounds(141, 115, 76, 22);
+		frame.getContentPane().add(cb1);
+		
+		JButton btnNewButton = new JButton("submit");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String id=t1.getText();
+				String name=t2.getText();
+				String branch=(String) cb1.getSelectedItem();
+				String marks=t3.getText();
+				int m=Integer.parseInt(marks);
+				try
+				{
+					java.sql.Connection con=DriverManager.getConnection("jdbs:mysql://localhost:3306/har","root","mrec");
+					String q="insert into students values('"+id+"','"+name+"','"+branch+"','"+m+"');"
+							Statement stn=con.createStatement();
+					stn.executeUpdate(q);
+					con.close();
+					JOptionPane.showMessageDialog(btnNewButton,"insertion success");
+				 }
+				catch(SQLException e1)
+				{
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnNewButton.setBounds(121, 201, 89, 23);
+		frame.getContentPane().add(btnNewButton);
+	}
+
+}
